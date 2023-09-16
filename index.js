6d7c71aa887e01dddeb58b83101d06fddc62a5c6
miguelmalo12 <miguelmalo12@gmail.com>
2023-09-16 13:28:47 -0700

    Script implemented, post endpoint writes the req in in.json, then runs script, script writes in out.json, then we read from out.json and send back res

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 const express = require('express');
 const cors = require('cors');
+const { exec } = require('child_process');
+const fs = require('fs');
 const app = express();
 
-// Import routes
-
-
-// Load environment variables from the .env file
 require("dotenv").config();
 
-// Fall back to 5051 if not provided
 const PORT = process.env.PORT || 5051;
 
 // Middleware
 app.use(cors({origin: process.env.CORS_ORIGIN}));
 app.use(express.json());
 
+const runShellCommand = (commandString) => new Promise((resolve, reject) => {
+
+	exec(commandString, (err, stdout, stdin) => {
+		if (err) {
+			console.error(stderr.toString());
+			return reject(err);
+		}
+		return resolve(stdout.toString());
+	});
+});
+
 // Routes
+app.post('/recommend', async (req, res) => {
+    const userPreferences = req.body;
+    
+    fs.writeFileSync('script/in/in.json', JSON.stringify(userPreferences));
 
+    const command = 'python3 script/lush.py';
 
+    console.log("Executing Python script");
+    runShellCommand(command).then( () => {
+        const output = JSON.parse(fs.readFileSync('script/out/out.json', 'utf-8'));
 
-app.listen(PORT, () => {
+        console.log("Sending response");
+        res.json(output);
+    }) .catch((err) => {
+        console.error(`Error executing script: ${err}`);
+        return res.status(500).json({ error: "Internal Server Error" });
+    })
+}
+);
+
+const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+server.timeout = 100000;
